b5924fcd6f2a05384633cc2c30e4a35a54cac58d
jimjam1337 <130655449+jimjam1337@users.noreply.github.com>
2023-06-26 10:33:19 +1000

    Update main.py
    fixede login function by removing email range definition from function, now need to figure out how to keep the web driver open after function call.

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -67,233 +67,176 @@ root.mainloop()
 
 # DIALOGUE BOX ENDS
 
-# DEFINE LOGIN FUNCTION
-logging.info("Beginning login function")
-print("login function defined")
+# DEFINE LOGIN FUNCTION BEGINS
+
 def Login_Function():
     print("begin login sequence")
-    # Define the base email and password
-    email_base = ["aga", "agapc"]
-    password = "AGA111222"
-    # Ask for the starting and ending values of the ranges
-    start_value_1 = simpledialog.askinteger("Input", "Enter the starting value for Xbox accounts [1]")
-    # Use the default value if the user didn't enter anything
-    if not start_value_1:
-        start_value_1 = 1
-    end_value_1 = simpledialog.askinteger("Input", "Enter the ending value for Xbox accounts [104]")
-    # Use the default value if the user didn't enter anything
-    if not end_value_1:
-        end_value_1 = 104
-    start_value_2 = simpledialog.askinteger("Input", "Enter the starting value for PC accounts [1]")
-    # Use the default value if the user didn't enter anything
-    if not start_value_2:
-        start_value_2 = 1
-    end_value_2 = simpledialog.askinteger("Input", "Enter the ending value for PC accounts [24]")
-    # Use the default value if the user didn't enter anything
-    if not end_value_2:
-        end_value_2 = 24
 
-    ranges = [(start_value_1, end_value_1), (start_value_2, end_value_2)]
-
-    # Loop through each base email and range
-    for email_base, r in zip(email_base, ranges):
-        # Loop through each account
-        for i in range(*r):
-            # Create the email address
-            email = email_base + str(i) + "@activegamers.com.au"
-
-            # log which account will be deactivated based on email variable
-            logging.info('Beginning deactivation of account %s', email)
-
-            # Create a new instance of the Chrome driver
-            driver = webdriver.Chrome()
-            print("driver opened")
-
-            # Navigate to the Microsoft login page
-            driver.get("https://account.microsoft.com/")
-
-            # Wait for the first signin field to be present and clickable
-            signin_field = WebDriverWait(driver, 500).until(
-                EC.element_to_be_clickable((By.ID, "id__4"))
-            )
-            print("first signin field clickable")
-
-            # Click the email field
-            signin_field.click()
-
-            # Wait for the email field to be present and clickable
-            email_field = WebDriverWait(driver, 500).until(
-                EC.element_to_be_clickable((By.ID, "i0116"))
-            )
-            print("second email field clickable")
-
-            # Send the email to the email field
-            email_field.send_keys(email)
-            email_field.send_keys(Keys.RETURN)
-
-            print("email sent")
-
-            # Wait for the password field to be present and clickable
-            password_field = WebDriverWait(driver, 500).until(
-                EC.element_to_be_clickable((By.ID, "i0118"))
-            )
-
-            print("password field clickable")
-
-            # Send the password to the email field
-            password_field.send_keys(password)
-            password_field.send_keys(Keys.RETURN)
-
-            print("password sent")
-
-            # CODE IN CASE UPDATE SECURITY INFO PAGE LOADS (case1)
-
-            try:
-                # Check if the 'iLooksGood' element is present and clickable
-                looks_good_field = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "iLooksGood")))
-                # if the element is clickable, send enter to the looks good field
-                looks_good_field.send_keys(Keys.RETURN)
-                logging.info("update security info window appeared and was bypassed")
-            except TimeoutException:
-                logging.info("update security info window did not appear")
-
-            # CODE IN CASE PRIVACY NOTICE PAGE LOADS (case2)
-
-            try:
-                # Waiting up to 10 seconds for the privacy notice page to load
-                WebDriverWait(driver, 10).until(EC.url_contains("https://privacynotice.account.microsoft.com/"))
-                logging.info("Waiting to see if privacy notice page appears")
-
-                # if URL contains "https://privacynotice.account.microsoft.com/", bypass and go to the main account page
-                driver.get("https://account.microsoft.com/")
-                logging.info(
-                    "privacy notice page 'https://privacynotice.account.microsoft.com/' appeared and was bypassed")
-
-            except TimeoutException:
-                # if the URL does not contain "https://privacynotice.account.microsoft.com/"
-                logging.info("privacy notice page 'https://privacynotice.account.microsoft.com/' did not appear")
-
-            # CODE IN CASE BREAK FREE OF YOUR PASSWORDS (UPSELL) PAGE LOADS (case3)
-
-            try:
-                # waiting for up to 10 seconds to see if the break free of your passwords page loads
-                WebDriverWait(driver, 10).until(EC.url_contains("https://account.live.com/apps/upsell?"))
-
-                # if the URL contains "https://account.live.com/apps/upsell?", skip it and go to the main account page
-                driver.get("https://account.microsoft.com/")
-                logging.info("Upsell page 'https://account.live.com/apps/upsell?' appeared and was bypassed")
-
-            except TimeoutException:
-                # if the URL does not contain "https://account.live.com/apps/upsell?"
-                logging.info("Upsell page 'https://account.live.com/apps/upsell?' did not appear")
-
-            # CODE IN CASE STAY SIGNED IN WINDOW APPEARS (case4)
-
-            try:
-                # Wait for the 'stayed signed in? no' field to be present and clickable
-                staysignedin_no_field = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.ID, "idBtn_Back"))
-                )
-
-                # send enter to the 'stay signed in? no' field
-                staysignedin_no_field.send_keys(Keys.RETURN)
-
-                logging.info("stay signed in window appeared and was completed")
-
-            except TimeoutException:
-                logging.info("stay signed in window did not appear")
-
-                # CODE IN CASE SECOND PRIVACY NOTICE PAGE LOADS (case5)
-
-            try:
-                # Waiting up to 10 seconds for the privacy notice page to load
-                WebDriverWait(driver, 10).until(EC.url_contains("https://privacynotice.account.microsoft.com/"))
-                logging.info("Waiting to see if privacy notice page appears")
-
-                # if URL contains "https://privacynotice.account.microsoft.com/", bypass and go to the main account page
-                driver.get("https://account.microsoft.com/")
-                logging.info(
-                    "privacy notice page 'https://privacynotice.account.microsoft.com/' appeared and was bypassed")
-
-            except TimeoutException:
-                # if the URL does not contain "https://privacynotice.account.microsoft.com/"
-                logging.info("privacy notice page 'https://privacynotice.account.microsoft.com/' did not appear")
-
-# DEACTIVATE CODE
-if result == "Deactivate":
-    print("Deactivate")
-
-    logging.info("Now beginning deactivation process")
-
-    Login_Function()
-
+    # Create a new instance of the Chrome driver
     driver = webdriver.Chrome()
+    print("driver opened")
+
+    # Navigate to the Microsoft login page
+    driver.get("https://account.microsoft.com/")
+
+    # Wait for the first signin field to be present and clickable
+    signin_field = WebDriverWait(driver, 500).until(
+        EC.element_to_be_clickable((By.ID, "id__4"))
+    )
+    print("first signin field clickable")
+
+    # Click the email field
+    signin_field.click()
+
+    # Wait for the email field to be present and clickable
+    email_field = WebDriverWait(driver, 500).until(
+        EC.element_to_be_clickable((By.ID, "i0116"))
+    )
+    print("second email field clickable")
+
+    # Send the email to the email field
+    email_field.send_keys(email)
+    email_field.send_keys(Keys.RETURN)
+
+    print("email sent")
+
+    # Wait for the password field to be present and clickable
+    password_field = WebDriverWait(driver, 500).until(
+        EC.element_to_be_clickable((By.ID, "i0118"))
+    )
+
+    print("password field clickable")
+
+    # Send the password to the email field
+    password_field.send_keys(password)
+    password_field.send_keys(Keys.RETURN)
+
+    print("password sent")
+
+    # CODE IN CASE UPDATE SECURITY INFO PAGE LOADS (case1)
 
     try:
-        # Wait for the manage link to be present and clickable
-        manage_link = WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.LINK_TEXT, "Manage"))
-        )
+        # Check if the 'iLooksGood' element is present and clickable
+        looks_good_field = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "iLooksGood")))
+        # if the element is clickable, send enter to the looks good field
+        looks_good_field.send_keys(Keys.RETURN)
+        logging.info("update security info window appeared and was bypassed")
+    except TimeoutException:
+        logging.info("update security info window did not appear")
 
-        # send enter to the manage link
-        manage_link.send_keys(Keys.RETURN)
+    # CODE IN CASE PRIVACY NOTICE PAGE LOADS (case2)
 
-        # Wait for the first cancel link to be present and clickable
-        cancel_link1 = WebDriverWait(driver, 500).until(
-        EC.element_to_be_clickable((By.ID, "cancel-sub-button"))
-        )
+    try:
+        # Waiting up to 10 seconds for the privacy notice page to load
+        WebDriverWait(driver, 10).until(EC.url_contains("https://privacynotice.account.microsoft.com/"))
+        logging.info("Waiting to see if privacy notice page appears")
 
-        print("cancel link 1 found")
-
-        # Use JavaScript to click the cancel link
-        driver.execute_script("arguments[0].click();", cancel_link1)
-
-        print("cancel link 1 clicked")
-
-        # Wait for the second cancel link (cancel button) to be present and clickable
-        cancel_link2 = WebDriverWait(driver, 500).until(
-        EC.element_to_be_clickable((By.ID, "benefit-cancel"))
-        )
-
-        print("cancel link 2 found")
-
-        # Use JavaScript to click the second cancel link
-        driver.execute_script("arguments[0].click();", cancel_link2)
-
-        # print message to indicate that second cancel link has been clicked
-        print("cancel link 2 clicked")
-
-        time.sleep(5)
-
-        actions = ActionChains(driver)
-        actions.send_keys(Keys.ARROW_DOWN)
-        print("down arrow sent")
-        actions.send_keys(Keys.TAB)
-        print("tab  sent")
-        actions.send_keys(Keys.RETURN)
-        print("return sent")
-        actions.perform()
-
-        # Close the browser window
-        driver.close()
-
-        print("browser closed")
-
-        # log the completed deactivation for the account
-        logging.info('Account %s has been completed', email)
-        logging.info("---------------------------------------------------------")
+        # if URL contains "https://privacynotice.account.microsoft.com/", bypass and go to the main account page
+        driver.get("https://account.microsoft.com/")
+        logging.info(
+            "privacy notice page 'https://privacynotice.account.microsoft.com/' appeared and was bypassed")
 
     except TimeoutException:
-        logging.info("Manage link for %s did not become clickable - probably the account is already deactivated")
-        logging.info("---------------------------------------------------------")
+        # if the URL does not contain "https://privacynotice.account.microsoft.com/"
+        logging.info("privacy notice page 'https://privacynotice.account.microsoft.com/' did not appear")
 
-# reactivate code
+    # CODE IN CASE BREAK FREE OF YOUR PASSWORDS (UPSELL) PAGE LOADS (case3)
+
+    try:
+        # waiting for up to 10 seconds to see if the break free of your passwords page loads
+        WebDriverWait(driver, 10).until(EC.url_contains("https://account.live.com/apps/upsell?"))
+
+        # if the URL contains "https://account.live.com/apps/upsell?", skip it and go to the main account page
+        driver.get("https://account.microsoft.com/")
+        logging.info("Upsell page 'https://account.live.com/apps/upsell?' appeared and was bypassed")
+
+    except TimeoutException:
+        # if the URL does not contain "https://account.live.com/apps/upsell?"
+        logging.info("Upsell page 'https://account.live.com/apps/upsell?' did not appear")
+
+    # CODE IN CASE STAY SIGNED IN WINDOW APPEARS (case4)
+
+    try:
+        # Wait for the 'stayed signed in? no' field to be present and clickable
+        staysignedin_no_field = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "idBtn_Back"))
+        )
+
+        # send enter to the 'stay signed in? no' field
+        staysignedin_no_field.send_keys(Keys.RETURN)
+
+        logging.info("stay signed in window appeared and was completed")
+
+    except TimeoutException:
+        logging.info("stay signed in window did not appear")
+
+        # CODE IN CASE SECOND PRIVACY NOTICE PAGE LOADS (case5)
+
+    try:
+        # Waiting up to 10 seconds for the privacy notice page to load
+        WebDriverWait(driver, 10).until(EC.url_contains("https://privacynotice.account.microsoft.com/"))
+        logging.info("Waiting to see if privacy notice page appears")
+
+        # if URL contains "https://privacynotice.account.microsoft.com/", bypass and go to the main account page
+        driver.get("https://account.microsoft.com/")
+        logging.info(
+            "second privacy notice page 'https://privacynotice.account.microsoft.com/' appeared and was bypassed")
+
+    except TimeoutException:
+        # if the URL does not contain "https://privacynotice.account.microsoft.com/"
+        logging.info("second privacy notice page 'https://privacynotice.account.microsoft.com/' did not appear")
+
+    print("login function completed")
+    logging.info("login function completed within login function")
+
+# DEFINE LOGIN FUNCTION ENDS
+
+logging.info("Beginning login function")
+print("login function defined")
+
+# DEFINE THE BASE EMAIL AND PASSWORD
+email_base = ["aga", "agapc"]
+password = "AGA111222"
+# Ask for the starting and ending values of the ranges
+start_value_1 = simpledialog.askinteger("Input", "Enter the starting value for Xbox accounts [1]")
+# Use the default value if the user didn't enter anything
+if not start_value_1:
+    start_value_1 = 1
+end_value_1 = simpledialog.askinteger("Input", "Enter the ending value for Xbox accounts [104]")
+# Use the default value if the user didn't enter anything
+if not end_value_1:
+    end_value_1 = 104
+start_value_2 = simpledialog.askinteger("Input", "Enter the starting value for PC accounts [1]")
+# Use the default value if the user didn't enter anything
+if not start_value_2:
+    start_value_2 = 1
+end_value_2 = simpledialog.askinteger("Input", "Enter the ending value for PC accounts [24]")
+# Use the default value if the user didn't enter anything
+if not end_value_2:
+    end_value_2 = 24
+
+ranges = [(start_value_1, end_value_1), (start_value_2, end_value_2)]
+
+print("define base email and password finished")
+logging.info("define base email and password finished")
+
+# Loop through each base email and range
+for email_base, r in zip(email_base, ranges):
+    # Loop through each account
+    for i in range(*r):
+        # Create the email address
+        email = email_base + str(i) + "@activegamers.com.au"
+
+        # log which account will be deactivated based on email variable
+        logging.info('Beginning deactivation of account %s', email)
+
+# REACTIVATE CODE
 if result == "Reactivate":
     print("Reactivate loop initiated")
 
     Login_Function()
-
-    driver = webdriver.Chrome()
+    print("login function completed outside login function")
+    logging.info("login function completed outside login function")
 
     # Navigate to the subscriptions page
     driver.get(
@@ -349,3 +292,71 @@ if result == "Reactivate":
     # script needs to confirm that susbcription has been usccessful at this point - could search for 'thank for joining' text
 
     print(email + " completed")
+
+    # DEACTIVATE CODE
+    if result == "Deactivate":
+        print("Deactivate")
+
+        logging.info("Now beginning deactivation process")
+
+        Login_Function()
+
+        driver = webdriver.Chrome()
+
+        try:
+            # Wait for the manage link to be present and clickable
+            manage_link = WebDriverWait(driver, 30).until(
+                EC.element_to_be_clickable((By.LINK_TEXT, "Manage"))
+            )
+
+            # send enter to the manage link
+            manage_link.send_keys(Keys.RETURN)
+
+            # Wait for the first cancel link to be present and clickable
+            cancel_link1 = WebDriverWait(driver, 500).until(
+                EC.element_to_be_clickable((By.ID, "cancel-sub-button"))
+            )
+
+            print("cancel link 1 found")
+
+            # Use JavaScript to click the cancel link
+            driver.execute_script("arguments[0].click();", cancel_link1)
+
+            print("cancel link 1 clicked")
+
+            # Wait for the second cancel link (cancel button) to be present and clickable
+            cancel_link2 = WebDriverWait(driver, 500).until(
+                EC.element_to_be_clickable((By.ID, "benefit-cancel"))
+            )
+
+            print("cancel link 2 found")
+
+            # Use JavaScript to click the second cancel link
+            driver.execute_script("arguments[0].click();", cancel_link2)
+
+            # print message to indicate that second cancel link has been clicked
+            print("cancel link 2 clicked")
+
+            time.sleep(5)
+
+            actions = ActionChains(driver)
+            actions.send_keys(Keys.ARROW_DOWN)
+            print("down arrow sent")
+            actions.send_keys(Keys.TAB)
+            print("tab  sent")
+            actions.send_keys(Keys.RETURN)
+            print("return sent")
+            actions.perform()
+
+            # Close the browser window
+            driver.close()
+
+            print("browser closed")
+
+            # log the completed deactivation for the account
+            logging.info('Account %s has been completed', email)
+            logging.info("---------------------------------------------------------")
+
+        except TimeoutException:
+            logging.info("Manage link for %s did not become clickable - probably the account is already deactivated")
+            logging.info("---------------------------------------------------------")
